d78add42af5cb2c0298f68348431f4d2fdb9d855
DengSinkiang <dengsinkiang@gmail.com>
2018-12-06 18:24:52 +0800

    Update range.go

--- src/range.go
+++ src/range.go
@@ -6,26 +6,26 @@ import (
 
 func main() {
 
-	nums := []int{1, 2, 3}
-	sum := 0
-	for _, num := range nums {
-		sum += num
-	}
-	fmt.Println("sum:", sum)
+    nums := []int{1, 2, 3}
+    sum := 0
+    for _, num := range nums {
+        sum += num
+    }
+    fmt.Println("sum:", sum)
 
-	for i, num := range nums {
-		if num == 3 {
-			fmt.Println("index:", i)
-		}
+    for i, num := range nums {
+        if num == 3 {
+	    fmt.Println("index:", i)
 	}
+    }
 
-	//range 在 map 中迭代键值对
-	values := map[string]string{"a": "boy", "b": "girl"}
-	for k, v := range values {
-		fmt.Printf("%s -> %s\n", k, v)
-	}
-	//range 可以只遍历map的键
-	for k := range values {
+    //range 在 map 中迭代键值对
+    values := map[string]string{"a": "boy", "b": "girl"}
+    for k, v := range values {
+	fmt.Printf("%s -> %s\n", k, v)
+    }
+    //range 可以只遍历map的键
+    for k := range values {
         fmt.Println("key:", k)
     }
 
