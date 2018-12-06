c8f42bc725503a26e8ea2644ae1f8b63cfb40d21
DengSinkiang <dengsinkiang@gmail.com>
2018-12-06 20:30:08 +0800

    xiugai

--- src/closures.go
+++ src/closures.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+//inSeq 返回在其内部定义的匿名函数 该返回的函数采用闭包的方式
 func intSeq() func() int {
 	i := 10
 	return func() int {
@@ -12,7 +13,8 @@ func intSeq() func() int {
 
 func main() {
 
-	//调用 ineSeq 函数将返回值赋值给 nextInt
+	//调用 ineSeq 函数将返回值赋值给 nextInt 该函数的值包含本身的值 i
+	//每次调用nextInt都会更新 i
 	nextInt := intSeq()
 
 	//多次调用 nextInt 查看闭包的值的变化
