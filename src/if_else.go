e890b213011f9661d93062f6326990728794de24
DengSinkiang <dengsinkiang@gmail.com>
2018-12-06 17:32:57 +0800

    Update if_else.go

--- src/if_else.go
+++ src/if_else.go
@@ -4,13 +4,13 @@ import "fmt"
 
 func main() {
 
-	if 5 % 2 == 0 {
-		fmt.Println("5 is even")
-	} else {
-		fmt.Println("5 is odd")
-	}
+    if 5 % 2 == 0 {
+	fmt.Println("5 is even")
+    } else {
+	fmt.Println("5 is odd")
+    }
 
-	if 4 % 2 == 0 {
+    if 4 % 2 == 0 {
         fmt.Println("4 is divisible by 2")
     }
     //在条件语句之前可以有一个声明语句,此变量可以在所有的条件分支中使用
