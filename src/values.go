5a6cff978bc9f5606aa977bc8a7ea1a4b80912f0
DengSinkiang <dengsinkiang@gmail.com>
2018-12-06 17:34:06 +0800

    Update values.go

--- src/values.go
+++ src/values.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("2+5 =", 2+5)
-	fmt.Println("7.0/2.0 =", 7.0/2.0)
+    fmt.Println("2+5 =", 2+5)
+    fmt.Println("7.0/2.0 =", 7.0/2.0)
     fmt.Println("hello" + "world")
     fmt.Println(true || false)
     fmt.Println(true && false)
