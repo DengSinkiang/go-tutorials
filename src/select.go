44dcbad8df31a1bff9e0a4595a4e42ab092fa160
DengSinkiang <dengsinkiang@gmail.com>
2018-12-10 09:36:23 +0800

    Update select.go

--- src/select.go
+++ src/select.go
@@ -26,9 +26,9 @@ func main() {
 		//select 关键字来同时等待这两个值
         select {
         case msg1 := <-c1:
-            fmt.Println("received", msg1)
+            	fmt.Println("received", msg1)
         case msg2 := <-c2:
-            fmt.Println("received", msg2)
+            	fmt.Println("received", msg2)
         }
     }
 
