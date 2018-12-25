1a2d762b820fe732185746d2f3724c96ae516315
ZJDM <dengsinkiang@gmail.com>
2018-12-25 15:12:36 +0800

    dxj

--- src/sorting_by_functions.go
+++ src/sorting_by_functions.go
@@ -8,19 +8,19 @@ type ByLength []string
 //实现了 sort.Interface 的 Len，Less 和 Swap 方法
 //就可以使用 sort 包的通用 Sort 方法
 func (s ByLength) Len() int {
-        return len(s)
+    return len(s)
 }
 
 func (s ByLength) Swap(i, j int) {
-        s[i], s[j] = s[j], s[i]
+    s[i], s[j] = s[j], s[i]
 }
 
 func (s ByLength) Less(i, j int) bool {
-        return len(s[i]) < len(s[j])
+    return len(s[i]) < len(s[j])
 }
 
 func main() {
-        fruits := []string{"peachewew", "rrrana", "kiwiewe"}
-        sort.Sort(ByLength(fruits))
-        fmt.Println(fruits)
+    fruits := []string{"peachewew", "rrrana", "kiwiewe"}
+    sort.Sort(ByLength(fruits))
+    fmt.Println(fruits)
 }
